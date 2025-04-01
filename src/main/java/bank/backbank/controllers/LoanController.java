11ac3651cc31a37f07a2d26d0bf995c69919fc47
LegacyAdrian <legacyadrian2.0@gmail.com>
2025-04-01 12:15:56 +0100

    Añadido Comentario

--- src/main/java/bank/backbank/controllers/LoanController.java
+++ src/main/java/bank/backbank/controllers/LoanController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequestMapping("/bank/")
 @CrossOrigin(origins = "http://localhost:5173")
 public class LoanController {
-
+    //Aqui el Service
     LoanService loanService;
 
     @Autowired
